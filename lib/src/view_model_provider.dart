47fb20c83f683e50b29706ddff7ce2afc52df20a
Shubham Gupta <shubham.developer16@gmail.com>
2023-01-11 12:53:29 +0530

    ViewModelProvider is now independent from provider

--- lib/src/view_model_provider.dart
+++ lib/src/view_model_provider.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/widgets.dart';
-import 'package:provider/provider.dart';
 import 'package:provider/single_child_widget.dart';
 
 import 'view_model.dart';
@@ -10,39 +9,65 @@ mixin ViewModelProviderSingleChildWidget on SingleChildWidget {}
 
 /// [ViewModelProvider] is used to wrap the widget with your custom [ViewModel].
 /// This requires [create] which accepts custom [ViewModel] and [child] Widget.
-class ViewModelProvider<T extends ViewModel> extends SingleChildStatelessWidget
+class ViewModelProvider<T extends ViewModel> extends SingleChildStatefulWidget
     with ViewModelProviderSingleChildWidget {
-  final T Function(BuildContext) _create;
+  final T Function(BuildContext context) create;
 
-  const ViewModelProvider({super.key, required T Function(BuildContext) create, super.child})
-      : _create = create;
+  const ViewModelProvider(
+      {super.key, required this.create, super.child});
 
-  @override
-  Widget buildWithChild(BuildContext context, Widget? child) {
-    return Provider<T>(
-      create: _create,
-      dispose: (_, viewModel) => viewModel.dispose(),
-      lazy: false,
-      child: child,
-    );
-  }
+  static F? maybeOf<F extends ViewModel>(BuildContext context) =>
+      context.dependOnInheritedWidgetOfExactType<_VMP<F>>()?.viewModel;
 
-  static T of<T extends ViewModel>(BuildContext context) {
-    try {
-      return Provider.of<T>(context, listen: false);
-    } on ProviderNotFoundException catch (e) {
-      if (e.valueType != T) rethrow;
-      throw FlutterError(
-        '''
-        ViewModelProvider.of() called with a context that does not contain a $T.
-        No ancestor could be found starting from the context that was passed to ViewModelProvider.of<$T>().
+  /// [ViewModelProvider].[of] method allows to get the custom [ViewModel] from anywhere nested inside [ViewModelProvider]'s [child]
+  static F of<F extends ViewModel>(BuildContext context) {
+    final res = maybeOf<F>(context);
+    assert(res != null, '''
+        ViewModelProvider.of() called with a context that does not contain a $F.
+        No ancestor could be found starting from the context that was passed to ViewModelProvider.of<$F>().
 
         This can happen if the context you used comes from a widget above the ViewModelProvider.
 
         The context used was: $context
-        ''',
-      );
-    }
+        ''');
+    return res!;
+  }
+
+  @override
+  State<ViewModelProvider<T>> createState() => _ViewModelProviderState<T>();
+}
+
+class _ViewModelProviderState<T extends ViewModel>
+    extends SingleChildState<ViewModelProvider<T>> {
+  late final T _viewModel;
+
+  @override
+  void initState() {
+    _viewModel = widget.create(context);
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    _viewModel.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget buildWithChild(BuildContext context, Widget? child) {
+    assert(child != null, "child must not be null");
+    return _VMP(viewModel: _viewModel, child: child!);
+  }
+}
+
+class _VMP<T extends ViewModel> extends InheritedWidget {
+  final T viewModel;
+
+  const _VMP({super.key, required this.viewModel, required super.child});
+
+  @override
+  bool updateShouldNotify(covariant _VMP oldWidget) {
+    return viewModel != oldWidget.viewModel;
   }
 }
 
